d07932a1cf97a7319e697870e41030ae01b4e276
xujiiii <herriaxujia@163.com>
2026-08-04 17:25:50 +0100

    Now it correctly copy the best checkpoint recorded in final.pkl to best.ckpt

--- Pipe/P3/rename_best_ckpt.py
+++ Pipe/P3/rename_best_ckpt.py
@@ -2,7 +2,8 @@ import argparse
 import re
 import shutil
 from pathlib import Path
-
+import pickle
+import os
 
 def main():
     ap = argparse.ArgumentParser()
@@ -10,8 +11,17 @@ def main():
     args = ap.parse_args()
 
     f3_out = Path(args.folder)
+    
 
-    ckpt_files = list(f3_out.glob("checkpoint_epoch*_loss*.ckpt"))
+    # 读取并载入 pkl 文件
+    with open(f"{f3_out}/final.pkl", 'rb') as f:
+        data = pickle.load(f)
+
+    print(f"Best loss is {data['best']}, total epochs is {data['epochs']}")    
+    
+    best_name=f"checkpoint_epoch*_loss{data['best']}.ckpt"
+    
+    ckpt_files = list(f3_out.glob(best_name))
     if not ckpt_files:
         print(f"No matching checkpoint files found in {f3_out}")
         return
